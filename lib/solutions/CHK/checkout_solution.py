ea76ef8ee45efb40a9afd70e69df6b1a2e15c721
sbx_user1051 <sbx_user1051@169.254.48.245>
2023-05-31 23:39:17 +0000

    Wed May 31 23:39:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,24 @@
+import string
+
+from lib.solutions.CHK.supermarket import SupermarketCheckout
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    supercheckout = SupermarketCheckout()
+
+    total_price = 0
+    items = skus.split(",")
+    for item in items:
+        item = item.strip()
+        item_name = item[-1]
+        item_count = int(item[:-1])
+
+        price = supercheckout.calculate_item_price(item_name, item_count)
+
+        if price == -1:
+            print("Invalid input")
+            break
+        total_price += price
+    return total_price
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,6 @@
+from lib.solutions.CHK import checkout_solution
+
+
+class TestChk:
+    def test_checkout(self):
+        assert checkout_solution.checkout("3A,2A") == 330
