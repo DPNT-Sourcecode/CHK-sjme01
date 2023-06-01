1118fe5029eaaa7d6d41474ac43a9ff38c49fb91
sbx_user1051 <sbx_user1051@169.254.48.245>
2023-06-01 02:32:37 +0000

    Thu Jun 01 02:32:37 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,9 +32,9 @@ def calculate_item_price(item, count):
                 offer_multiplier = count // quantity
                 remaining_items = count % quantity
                 return offer_multiplier * offer_price + remaining_items * pricing_table[item]
-            elif count == 3 and item == "A":
+            elif count == quantity == 3 and item == "A":
                 return offer_price
-            elif count == 5 and item == "A":
+            elif count == quantity == 5 and item == "A":
                 return offer_price
             elif count == quantity:
                 return offer_price
@@ -70,6 +70,7 @@ def checkout(skus) -> int:
     else:
         return -1
     return total_price
+
 
 
 
