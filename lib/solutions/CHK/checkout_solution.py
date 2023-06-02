c3a1d63511490fa3d5327d2dfe6859041e4820d0
sbx_user1051 <sbx_user1051@169.254.197.121>
2023-06-02 02:25:04 +0000

    Fri Jun 02 02:25:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -154,7 +154,7 @@ def checkout(skus) -> int:
 
             # elif item in ["S", "T", "X", "Y", "Z"] and len(set(skus)) == 1:
             #     return calculate_item_price(item, item_count)
-            elif item == "S" or item == "T" or item == "X" or item == "Y" or item == "Z" and item_count == skus.count("T") >= 1 or item_count == skus.count(
+            elif item == "S" or item == "T" or item == "X" or item == "Y" or item == "Z" and  len(set(skus)) == 3 and len(skus) == 4 and item_count == skus.count("T") >= 1 or item_count == skus.count(
                     "X") or item_count == skus.count("Y") >= 1 or item_count == skus.count("Z") >= 1 or item_count == skus.count("S") >= 1:
                 price = item_count * 15
 
@@ -184,4 +184,5 @@ def checkout(skus) -> int:
     else:
         return -1
     return total_price
+
 
