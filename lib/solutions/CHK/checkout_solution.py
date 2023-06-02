82d96138c07a704d21d0fd8a05908e347dce16ff
sbx_user1051 <sbx_user1051@169.254.197.121>
2023-06-02 00:51:49 +0000

    Fri Jun 02 00:51:49 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -132,13 +132,15 @@ def checkout(skus) -> int:
                 discount = 15
                 price = 2 * discount
             elif item in ["S", "T", "X", "Y", "Z"] and len(set(skus)) == 1:
-                return calculate_item_price(item, item_count)
+                return item_count * 15
+            # elif item in ["S", "T", "X", "Y", "Z"] and len(set(skus)) == 1:
+            #     return calculate_item_price(item, item_count)
             # elif item == "S" and item_count > skus.count("T") >= 1 or item_count > skus.count(
             #         "X") or item_count > skus.count("Y") >= 1 or item_count > skus.count("Z") >= 1:
             #     price = calculate_item_price(item, item_count)
-            elif item == "S" and item_count == skus.count("T") >= 1 or item_count == skus.count(
-                    "X") or item_count == skus.count("Y") >= 1 or item_count == skus.count("Z") >= 1:
-                price = item_count * 15
+            # elif item in ["S", "T", "X", "Y", "Z"] and item_count == skus.count("T") >= 0 or item_count == skus.count(
+            #         "X") or item_count == skus.count("Y") >= 1 or item_count == skus.count("Z") >= 1 and len(set(skus)) == 1:
+            #     return item_count * 15
             # elif item in ["S", "T", "X", "Y", "Z"] and item_count == skus.count("T") == skus.count("X") == skus.count(
             #         "Y") == skus.count("Z") == skus.count("S") and len(skus) >= 3:
             #     price = item_count * 15
@@ -159,7 +161,3 @@ def checkout(skus) -> int:
     else:
         return -1
     return total_price
-
-
-
-
