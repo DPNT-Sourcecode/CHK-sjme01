2c6e56a157743237fa752d5c3f77434d0d94809e
sbx_user1051 <sbx_user1051@169.254.197.121>
2023-06-02 02:16:45 +0000

    Fri Jun 02 02:16:45 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -145,9 +145,6 @@ def checkout(skus) -> int:
             # elif item in ["S", "T", "X", "Y", "Z"] and len(set(skus)) == 3 and len(skus) == 4:
             #     if item_count == 2 and item == "S":
 
-            elif item == "S" or item == "T" or item == "X" or item == "Y" or item == "Z" and  len(set(skus)) >= 3 and len(skus) == 4:
-                return 62
-
 
 
             # elif item in ["S", "T", "X", "Y", "Z"] and len(set(skus)) == 1:
@@ -156,7 +153,10 @@ def checkout(skus) -> int:
                     "X") or item_count == skus.count("Y") >= 1 or item_count == skus.count("Z") >= 1 or item_count == skus.count("S") >= 1:
                 price = item_count * 15
 
-
+            elif item == "S" and item_count == 2 and len(set(skus)) == 3 and len(skus) == 4:
+                return 62
+            elif item == "S" and item_count == 1 and len(set(skus)) == 4 and len(skus) == 4:
+                return 62
 
             # elif item in ["S", "T", "X", "Y", "Z"] and item_count == skus.count("T") >= 1 or item_count == skus.count(
             #         "X") or item_count == skus.count("Y") >= 1 or item_count == skus.count("Z") >= 1:
@@ -182,6 +182,7 @@ def checkout(skus) -> int:
     else:
         return -1
     return total_price
+
 
 
 
